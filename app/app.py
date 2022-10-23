77265921d4ebe5f71a545edc1110a9188ea3d72b
Philipp <pskudlik@gmail.com>
2022-10-23 02:35:31 +0200

    added ui

--- app/app.py
+++ app/app.py
@@ -1,44 +1,65 @@
 import os
-import sys
-import random
 from PySide6 import QtCore, QtWidgets, QtGui
+from PySide6.QtUiTools import QUiLoader
 
+from src.cmd.update_backlog import main as update_backlog
 from src.cmd.swiper import main as swiper
 from src.cmd.texter import main as texter
+from src.cmd.login import main as login
 
-class MyWidget(QtWidgets.QWidget):
+from dotenv import load_dotenv
+import configparser
+
+load_dotenv()
+config = configparser.ConfigParser()
+config.read(os.environ["LOVEBOT_CONFIG"])
+loader = QUiLoader()
+path_prefix = config['DEFAULT']["PathPrefix"]
+
+class MyApp(QtWidgets.QApplication):
     def __init__(self):
         super().__init__()
-
-        self.hello = ["Hallo Welt", "Hei maailma", "Hola Mundo", "Привет мир"]
-
-        self.run_swiper_btn = QtWidgets.QPushButton("Run Swiper!")
-        self.run_texter_btn = QtWidgets.QPushButton("Run Texter!")
-
-        self.text = QtWidgets.QLabel("Hello World",
-                                     alignment=QtCore.Qt.AlignCenter)
-
-        self.layout = QtWidgets.QVBoxLayout(self)
-        self.layout.addWidget(self.text)
-        self.layout.addWidget(self.run_swiper_btn)
-        self.layout.addWidget(self.run_texter_btn)
-
-        self.run_swiper_btn.clicked.connect(self.run_swiper)
-        self.run_texter_btn.clicked.connect(self.run_texter)
+        self.ui = loader.load(path_prefix+"assets/lovebot.ui", None)
+        self.ui_setup()
+        self.connection_setup()
+    
+    @QtCore.Slot()
+    def run_update_backlog(self):
+        update_backlog()
 
     @QtCore.Slot()
     def run_swiper(self):
-        swiper()
-    
+        #self.ui.status_label.setText("I'm swiping...")
+        run_report = swiper()
+        self.ui.status_label.setText(run_report)
+
     @QtCore.Slot()
     def run_texter(self):
-        texter()
+        #self.ui.status_label.setText("I'm texting...")
+        run_report = texter()
+        self.ui.status_label.setText(run_report)
+
+    @QtCore.Slot()
+    def run_login(self):
+        login()
+        self.ui.status_label.setText("Alright, let's go!")
+
+    def ui_setup(self):
+        #central_image
+        pixmap = QtGui.QPixmap(path_prefix+"assets/logo_small_centered.png")
+        self.ui.central_image.setPixmap(pixmap)
+        #favicon
+        my_icon = QtGui.QIcon()
+        my_icon.addFile(path_prefix+"assets/favicon.ico")
+        self.ui.setWindowIcon(my_icon)
+
+    def connection_setup(self):
+        self.ui.swipe_btn.clicked.connect(self.run_swiper)
+        self.ui.text_btn.clicked.connect(self.run_texter)
+        self.ui.login_btn.clicked.connect(self.run_login)
+
 
 if __name__ == "__main__":
-    app = QtWidgets.QApplication([])
-
-    widget = MyWidget()
-    widget.resize(800, 600)
-    widget.show()
-
-    sys.exit(app.exec())
+    app = MyApp()
+    app.ui.show()
+    app.exec()
